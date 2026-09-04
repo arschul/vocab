d4929de1223babe5633687b26afb5b9b5cc985e6
arschul <andersonschultz01@gmail.com>
2026-09-04 19:25:35 -0300

    Add data/a1-clothes.js

---
+++ data/a1-clothes.js
@@ -0,0 +1,31 @@
+window.VOCAB_DATA = window.VOCAB_DATA || {};
+window.VOCAB_DATA['a1/clothes'] = {
+  level:'a1', slug:'clothes', title:'Clothes', emoji:'\u{1F455}',
+  words:[
+    {w:'shirt',pos:'noun',e:'\u{1F455}',d:'clothes for the top of your body with buttons',x:'He wears a white shirt to work.'},
+    {w:'T-shirt',pos:'noun',e:'\u{1F455}',d:'a simple shirt with short arms',x:'I wear a T-shirt in summer.'},
+    {w:'pants',pos:'noun',e:'\u{1F456}',d:'clothes for your legs',x:'These pants are too long.'},
+    {w:'jeans',pos:'noun',e:'\u{1F456}',d:'strong blue pants',x:'She wears jeans every day.'},
+    {w:'dress',pos:'noun',e:'\u{1F457}',d:'one piece of clothing for a girl or woman',x:'That dress is beautiful.'},
+    {w:'skirt',pos:'noun',e:'\u{1F459}',d:'clothes that hang from the waist',x:'Her skirt is black.'},
+    {w:'jacket',pos:'noun',e:'\u{1F9E5}',d:'short clothes you wear over a shirt',x:'Take a jacket, it is cold.'},
+    {w:'coat',pos:'noun',e:'\u{1F9E5}',d:'long warm clothes for outside',x:'My coat is very warm.'},
+    {w:'sweater',pos:'noun',e:'\u{1F9F6}',d:'warm soft clothes for your top',x:'I need a sweater today.'},
+    {w:'shoes',pos:'noun',e:'\u{1F45F}',d:'things you wear on your feet',x:'My shoes are new.'},
+    {w:'socks',pos:'noun',e:'\u{1F9E6}',d:'soft things you wear under shoes',x:'Where are my socks?'},
+    {w:'hat',pos:'noun',e:'\u{1F452}',d:'something you wear on your head',x:'He wears a hat in the sun.'},
+    {w:'cap',pos:'noun',e:'\u{1F9E2}',d:'a soft hat with a front part',x:'His cap is red and blue.'},
+    {w:'scarf',pos:'noun',e:'\u{1F9E3}',d:'clothes you put around your neck',x:'She wears a scarf in winter.'},
+    {w:'gloves',pos:'noun',e:'\u{1F9E4}',d:'clothes for your hands',x:'Put your gloves on.'},
+    {w:'boots',pos:'noun',e:'\u{1F97E}',d:'tall shoes for rain or cold',x:'I wear boots when it rains.'},
+    {w:'shorts',pos:'noun',e:'\u{1FA73}',d:'short pants for hot days',x:'We wear shorts at the beach.'},
+    {w:'uniform',pos:'noun',e:'\u{1F393}',d:'special clothes for school or work',x:'Our school uniform is blue.'},
+    {w:'belt',pos:'noun',e:'\u{1FA71}',d:'a long band around your waist',x:'He needs a new belt.'},
+    {w:'bag',pos:'noun',e:'\u{1F45C}',d:'something you carry things in',x:'My bag is very heavy.'},
+    {w:'glasses',pos:'noun',e:'\u{1F453}',d:'things you wear to see better',x:'She wears glasses to read.'},
+    {w:'watch',pos:'noun',e:'\u231A',d:'a small clock you wear',x:'My watch is broken.'},
+    {w:'wear',pos:'verb',e:'\u{1F9CD}',d:'to have clothes on your body',x:'What do you wear to parties?'},
+    {w:'size',pos:'noun',e:'\u{1F4CF}',d:'how big or small something is',x:'What size are your shoes?'},
+    {w:'pocket',pos:'noun',e:'\u{1F45D}',d:'a small bag inside your clothes',x:'The key is in my pocket.'}
+  ]
+};
