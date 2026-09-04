895f6164cac6cc110e80b2c903be38cc1827979a
arschul <andersonschultz01@gmail.com>
2026-09-04 19:25:37 -0300

    Add data/a1-daily-routine.js

---
+++ data/a1-daily-routine.js
@@ -0,0 +1,31 @@
+window.VOCAB_DATA = window.VOCAB_DATA || {};
+window.VOCAB_DATA['a1/daily-routine'] = {
+  level:'a1', slug:'daily-routine', title:'Daily Routine', emoji:'\u23F0',
+  words:[
+    {w:'wake up',pos:'verb',e:'\u{1F634}',d:'to stop sleeping',x:'I wake up at six o\u2019clock.'},
+    {w:'get up',pos:'verb',e:'\u{1F6CF}\uFE0F',d:'to leave your bed',x:'She gets up very early.'},
+    {w:'wash',pos:'verb',e:'\u{1F9FC}',d:'to clean something with water',x:'I wash my hands before dinner.'},
+    {w:'brush',pos:'verb',e:'\u{1FAA5}',d:'to clean with a small brush',x:'Brush your teeth twice a day.'},
+    {w:'shower',pos:'noun',e:'\u{1F6BF}',d:'washing your body under water',x:'I take a shower after school.'},
+    {w:'dress',pos:'verb',e:'\u{1F455}',d:'to put clothes on',x:'He dresses very quickly.'},
+    {w:'eat',pos:'verb',e:'\u{1F37D}\uFE0F',d:'to put food in your mouth',x:'We eat together every night.'},
+    {w:'drink',pos:'verb',e:'\u{1F942}',d:'to take liquid into your mouth',x:'Drink more water in summer.'},
+    {w:'go',pos:'verb',e:'\u{1F6B6}',d:'to move to another place',x:'I go to school by bus.'},
+    {w:'come',pos:'verb',e:'\u{1F6AA}',d:'to move to where someone is',x:'Please come home before nine.'},
+    {w:'work',pos:'verb',e:'\u{1F4BC}',d:'to do a job',x:'My parents work in an office.'},
+    {w:'study',pos:'verb',e:'\u{1F4DA}',d:'to learn about something',x:'I study English on Tuesdays.'},
+    {w:'read',pos:'verb',e:'\u{1F4D6}',d:'to look at words and understand them',x:'She reads before she sleeps.'},
+    {w:'write',pos:'verb',e:'\u270D\uFE0F',d:'to put words on paper',x:'Write your name here, please.'},
+    {w:'play',pos:'verb',e:'\u26BD',d:'to do something for fun',x:'We play games after class.'},
+    {w:'watch',pos:'verb',e:'\u{1F4FA}',d:'to look at something for a time',x:'They watch TV in the evening.'},
+    {w:'listen',pos:'verb',e:'\u{1F3A7}',d:'to give attention to a sound',x:'I listen to music on the bus.'},
+    {w:'sleep',pos:'verb',e:'\u{1F6CC}',d:'to rest with your eyes closed at night',x:'Children sleep nine hours.'},
+    {w:'cook',pos:'verb',e:'\u{1F468}\u200D\u{1F373}',d:'to make food hot and ready to eat',x:'My dad cooks on Saturdays.'},
+    {w:'clean',pos:'verb',e:'\u{1F9F9}',d:'to take dirt away from something',x:'I clean my room on Sunday.'},
+    {w:'walk',pos:'verb',e:'\u{1F6B6}\u200D\u2640\uFE0F',d:'to move with your legs, not fast',x:'We walk to school together.'},
+    {w:'run',pos:'verb',e:'\u{1F3C3}',d:'to move with your legs very fast',x:'He runs in the park every morning.'},
+    {w:'start',pos:'verb',e:'\u25B6\uFE0F',d:'to begin something',x:'Classes start at eight.'},
+    {w:'finish',pos:'verb',e:'\u{1F3C1}',d:'to come to the end of something',x:'I finish my homework at six.'},
+    {w:'rest',pos:'verb',e:'\u{1F6CB}\uFE0F',d:'to stop working and relax',x:'Rest for ten minutes.'}
+  ]
+};
